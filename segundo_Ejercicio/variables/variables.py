cba7767a4764d588d14c1c7f187519815cd1c59d
RuthDanielaAguirre <argdiee@icloud.com>
2024-05-16 15:02:32 +0200

    first part of exercises

---
+++ segundo_Ejercicio/variables/variables.py
@@ -0,0 +1,9 @@
+#using variables
+myValue = 1
+print(myValue)
+
+#getting de type of the variable using type()
+print(type(myValue))
+
+#combine numbers and text using str() converting int data type into string data type
+print(str(myValue) + " is of the data type " + str(type(myValue)))
