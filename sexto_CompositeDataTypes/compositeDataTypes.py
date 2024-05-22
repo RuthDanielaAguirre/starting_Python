5e8458cb3e0ec5fe96c650dfd1196c026b88448b
RuthDanielaAguirre <argdiee@icloud.com>
2024-05-22 11:17:34 +0200

    adding the 6th

--- sexto_CompositeDataTypes/compositeDataTypes.py
+++ sexto_CompositeDataTypes/compositeDataTypes.py
@@ -1,6 +1,11 @@
 #a Composite data type is any data type that comprises primitive data types.
 
-#firs define a dictionary that will serve as the composite type for reading tabular data
+#Comma-separated values (CSV) is a file format that's used to store tabular data, such as data from a spreadsheet. You will work with the CSV file from the following block.
+
+#first define a dictionary that will serve as the composite type for reading tabular data
+
+import csv
+import copy
 
 myVehicle = {
     "vin" : "<empty>",
@@ -27,3 +32,36 @@ for key, value in myVehicle.items():
     print("{} : {}".format(key,value))
 
 myInventoryList = []
+
+#with "with open" syntax statement keeps a file open while you read data. It will automatically close the CSV file when the code inside the "with" block is finished running. Instead of using double quotation marks and .format to pass in the variables, you can use a single quotation mark and write in the variables between the "{}" symbols.
+
+# csv.reader() is a function that you are using from the csv library that you imported with the import csv statement.
+
+with open('car_fleet.csv') as csvFile:
+    csvReader = csv.reader(csvFile, delimiter=',')  
+    lineCount = 0  
+    for row in csvReader:
+        if lineCount == 0:
+            print(f'Column names are: {", ".join(row)}')  
+            lineCount += 1  
+        else:  
+            print(f'vin: {row[0]} make: {row[1]}, model: {row[2]}, year: {row[3]}, range: {row[4]}, topSpeed: {row[5]}, zeroSixty: {row[6]}, mileage: {row[7]}')  
+            currentVehicle = copy.deepcopy(myVehicle)  
+            currentVehicle["vin"] = row[0]  
+            currentVehicle["make"] = row[1]  
+            currentVehicle["model"] = row[2]  
+            currentVehicle["year"] = row[3]  
+            currentVehicle["range"] = row[4]  
+            currentVehicle["topSpeed"] = row[5]  
+            currentVehicle["zeroSixty"] = row[6]  
+            currentVehicle["mileage"] = row[7]  
+            myInventoryList.append(currentVehicle)  
+            lineCount += 1  
+    print(f'Processed {lineCount} lines.')
+    
+    for myCarProperties in myInventoryList:
+      for key, value in myCarProperties.items():
+        print("{} : {}".format(key,value))
+        print("-----")
+
+#By default, Python does a shallow copy of complex data types. A shallow copy refers, or points, to the storage location of the myVehicle dictionary variable. Without this line, you would have only one storage box, and only the last item in the list would be copied into memory. This line makes sure that new storage boxes are created in memory to store the new tabular data that is being read.
